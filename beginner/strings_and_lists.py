03564ae27663ab62f673f1f3ca6031174069df11
Aaron Li <aaronli39@gmail.com>
2018-10-02 22:12:16 -0400

    did 1

---
+++ beginner/strings_and_lists.py
@@ -0,0 +1,8 @@
+def str(input, a, b ,c ,d):
+    return input[a: b + 1] + " " + input[c: d + 1]
+
+print(str("Ro8LuhClKjf2MqKCQjNaVOobpjg4qMOnYV7RmgPpBxWEzyZW4u7NlysxVe9evNpE1dW7zup9zsUA6Halichoerus0h6PIyQJDBCAscalarisJjm8U4WiuPsOD8JQHtP17nh1cuEq3Gl7O0RF62viUevaXcNVuGePKAM",
+77,
+87,
+100,
+107))
